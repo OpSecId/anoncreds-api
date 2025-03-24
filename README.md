0e0c133bb0a8d7b2c73477f48bc543eeb74c9898
PatStLouis <patrick.st-louis@opsecid.ca>
2025-03-24 18:58:35 +0000

    add readme
    Signed-off-by: PatStLouis <patrick.st-louis@opsecid.ca>

--- README.md
+++ README.md
@@ -1,1 +1,252 @@
-# anoncreds-api
+# AnonCreds V2 API
+
+## Setup
+```
+docker build -t anoncreds-api .
+docker run anoncreds-api -p 8000:8000
+```
+
+## Usage
+
+### (ISSUER) Creating a credential schema
+
+To create a credential schema, we can use a json schema as input and add claim validators using the `minimum`, `maximum`, `minLength`, `maxlength` and `enum` keys on the properties. The `type` of the property can be used to specify if it's a `Hashed`(string) or `Number`(number) claim.
+
+#### Json Schema
+```json
+{
+    "title": "Sample Credential",
+    "description": "A sample credential",
+    "properties": {
+      "name": {
+        "type": "string"
+      }
+    }
+}
+```
+
+```bash
+curl -X 'POST' \
+  'http://localhost:8000/resources/schemas/credentials' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "jsonSchema": {
+    "title": "Sample Credential",
+    "description": "A sample credential",
+    "properties": {
+      "name": {
+        "type": "string"
+      }
+    }
+  },
+  "options": {}
+}'
+```
+
+### (ISSUER) Creating a verification method
+
+Once the schema is created, a verification method can be generated for issuing this credential. This will add a `verificationMethod` to the issuer's DID Document with a `credentialRegistry` property, resolving to the full AnonCreds public issuer object.
+
+```bash
+curl -X 'POST' \
+  'http://localhost:8000/resources/issuers/demo' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "credSchemaId": "zQmNb6UsQsMjTbE98wn2AFPVuviG4cCgEXX8WRyzYyUPhjF"
+}'
+```
+
+### (VERIFIER) Creating a presentation schema
+
+To create a presentation schema, there are 2 vp-request-2024 presentation query types defined below.
+
+#### Signature Query
+This is used for requesting `Signature`, `Commitment`, `Range` or `Encryption` statements.
+```json
+{
+  "type": "SignatureQuery",
+  // REQUIRED identifier for the signature statement proof
+  "referenceId": "sample-signature-request",
+  // REQUIRED verification method restriction for the source credential
+  "verificationMethod": "...",
+  // OPTIONAL claims to disclose in the presentation
+  "disclosed": [],
+  // OPTIONAL claim commitment statement request
+  "commitment": [
+    {
+      "claimRef": "name",
+      "messageGenerator": "...",
+      "blinderGenerator": "...",
+      // OPTIONAL range statement on the commitment, must be a number claim
+      "range": { 
+        "lower": 0,
+        "upper": 0
+      }
+    }
+  ],
+  // OPTIONAL encryption statement request
+  "encryption": [
+    {
+      "claimRef": "credentialId",
+      "domain": "example.com",
+      "encryptionKey": "..."
+    }
+  ]
+}
+```
+
+#### Equality Query
+This is used for requesting `Equality` statements from defined `Signature` statement claims.
+```json
+{
+  "type": "EqualityQuery",
+  // REQUIRED identifier for the equality statement proof
+  "referenceId": "are-names-equal",
+  // REQUIRED claims for the equality request
+  "claims": [
+    {
+      // REQUIRED claim label
+      "claimRef": "name",
+      // REQUIRED identifier for the signature statement proof
+      "signatureRef": "sample-signature-request"
+    }
+  ]
+}
+```
+
+```bash
+curl -X 'POST' \
+  'http://localhost:8000/resources/schemas/presentations' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "query": [
+    {
+      "type": "SignatureQuery",
+      "referenceId": "sample-signature-request",
+      "verificationMethod": "..."
+    }
+  ]
+}'
+```
+
+### (HOLDER) Requesting a credential
+Holder derives a request proof from the `subjectId` value they want to bind this credential to. This value will not be revealed to the issuer but a hash (`Scalar`) will be signed. This is a blinded claim acting as the link secret.
+```bash
+curl -X 'POST' \
+  'https://api.anoncreds.vc/operations/credentials/request' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "subjectId": "did:key:z6MkiTBz1ymuepAQ4HEHYSF1H8quG5GLVVQR3djdX3mDooWp",
+  "verificationMethod": "..."
+}'
+```
+
+### (Issuer) Issuing a credential
+
+The issuer can issue a credential by providing the `credentialSubject` as the key:value pairs for each `Hashed` and `Number` claims from the credential schema.
+A `revocationId` can also be passed as the `Revocation` claim value and a `requestProof` for the blinded `Scalar` claims.
+
+```bash
+curl -X 'POST' \
+  'https://api.anoncreds.vc/operations/credentials/issuance' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "credentialSubject": {
+    "name": "Alice"
+  },
+  "options": {
+    "credentialId": "...",
+    "revocationId": "...",
+    "requestProof": "...",
+    "verificationMethod": "..."
+  }
+}'
+```
+
+### (Holder) Storing a credential
+An issued credential and a `subjectId` can be provided to unblind the credential and store it.
+
+```bash
+curl -X 'POST' \
+  'https://api.anoncreds.vc/operations/credentials/storage' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "credential": {},
+  "subjectId": "did:key:z6MkiTBz1ymuepAQ4HEHYSF1H8quG5GLVVQR3djdX3mDooWp"
+}'
+```
+
+### (Holder) Creating a presentation
+To create a holder, the `subjectId` linking the credentials as well as a `challenge` and a `presentationSchemaId` can be provided.
+
+```bash
+curl -X 'POST' \
+  'https://api.anoncreds.vc/operations/presentations/creation' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "subjectId": "did:key:z6MkiTBz1ymuepAQ4HEHYSF1H8quG5GLVVQR3djdX3mDooWp",
+  "options": {
+      "challenge": "...",
+      "presSchemaId": "..."
+  }
+}'
+```
+
+### (Verifier) Verifying a presentation
+
+```bash
+curl -X 'POST' \
+  'https://api.anoncreds.vc/operations/presentations/verification' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "presentation": {},
+  "options": {
+    "challenge": "string",
+    "presSchemaId": "string"
+  }
+}'
+```
+
+## Utilities
+
+### Creating an encryption keypair
+```bash
+curl -X 'POST' \
+  'https://api.anoncreds.vc/utilities/keys' \
+  -H 'accept: application/json' \
+  -d ''
+```
+
+### Generating a domain proof generator
+```bash
+curl -X 'POST' \
+  'https://api.anoncreds.vc/utilities/generator' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "domain": "example.com"
+}'
+```
+
+### Validating a verifiable encryption
+Provide the `VerifiableEncryption` proof and the associated decryption key.
+```bash
+curl -X 'POST' \
+  'https://api.anoncreds.vc/utilities/decrypt' \
+  -H 'accept: application/json' \
+  -H 'Content-Type: application/json' \
+  -d '{
+  "proof": {},
+  "options": {
+    "decryptionKey": "..."
+  }
+}'
+```
--- demo/app/plugins/anoncreds.py
+++ demo/app/plugins/anoncreds.py
@@ -2,6 +2,7 @@ from config import Config
 from app.plugins.askar import AskarStorage
 from app.utils import zalgo_id
 import requests
+import uuid
 import json
 
 
@@ -11,9 +12,13 @@ class AnonCredsApi:
 
     async def provision(self):
         askar = AskarStorage()
-        await askar.provision()
+        await askar.provision(recreate=True)
         if await askar.fetch("demo", "default"):
             return
+
+        with open("app/static/demo/holder.json", "r") as f:
+            holder = json.loads(f.read())
+        holder_id = holder.get("subjectId")
 
         with open("app/static/demo/credentials/credit-card.json", "r") as f:
             cc_demo = json.loads(f.read())
@@ -24,139 +29,158 @@ class AnonCredsApi:
         )
 
         # Create Schema
-        cc_schema = self.create_cred_schema(cc_demo.get("schema")).get("schema")
+        # cc_schema_id = self.create_cred_schema(cc_demo.get("schema")).get("credentialSchemaId")
+        # cc_issuer = self.setup_issuer(cc_schema_id)
+        # cc_cred_def = cc_issuer.get("public")
 
-        cc_issuer = self.setup_issuer(cc_schema.get("id"))
-        cc_cred_def = cc_issuer.get("public")
-
+        cc_verification_method = cc_demo.get("issuer").get('verificationMethod')
         cc_cred_subject = cc_demo.get("credential").get("credentialSubject")
-        cc_credential = self.issue_credential(cc_cred_def.get("id"), cc_cred_subject)
-
+        cc_request_proof = self.request_credential(holder_id, cc_verification_method).get('requestProof')
+        cc_credential = self.issue_credential(
+            cred_id=str(uuid.uuid4()),
+            rev_id=str(uuid.uuid4()),
+            cred_subject=cc_cred_subject,
+            request_proof=cc_request_proof,
+            verification_method=cc_verification_method, 
+        )
+        
         with open("app/static/demo/credentials/rebates-card.json", "r") as f:
             rc_demo = json.loads(f.read())
 
-        rc_schema = self.create_cred_schema(rc_demo.get("schema")).get("schema")
-        rc_issuer = self.setup_issuer(rc_schema.get("id"))
-        rc_cred_def = rc_issuer.get("public")
+        # rc_schema = self.create_cred_schema(rc_demo.get("schema")).get("schema")
+        # rc_issuer = self.setup_issuer(rc_schema.get("id"))
+        # rc_cred_def = rc_issuer.get("public")
 
+        rc_verification_method = rc_demo.get("issuer").get('verificationMethod')
         rc_cred_subject = rc_demo.get("credential").get("credentialSubject")
         rc_cred_subject["clientNo"] = zalgo_id(64)
-        rc_credential = self.issue_credential(rc_cred_def.get("id"), rc_cred_subject)
-
-        cc_pres_statements = {
-            # 'signature': {
-            #     'disclosed': ['cvv']
-            # },
-            "revocation": {"accumulator": ""},
-            "encryption": {
-                "claim": "number",
-                "domain": cc_demo.get("issuer").get("domain"),
-                "encryptionKey": cc_cred_def.get("verifiable_encryption_key"),
-            },
-        }
-        challenge = self.create_nonce().get("nonce")
-        cc_pres_schema = self.create_pres_schema(
-            cc_cred_def.get("id"), cc_pres_statements
-        ).get("presReq")
-        cc_presentation = self.create_presentation(
-            cc_credential, cc_pres_schema.get("id"), challenge
-        ).get("presentation")
-        cc_verified = self.verify_presentation(
-            cc_presentation, cc_pres_schema.get("id"), challenge
+        rc_request_proof = self.request_credential(holder_id, rc_verification_method).get('requestProof')
+        rc_credential = self.issue_credential(
+            cred_id=str(uuid.uuid4()),
+            rev_id=str(uuid.uuid4()),
+            cred_subject=rc_cred_subject,
+            request_proof=rc_request_proof,
+            verification_method=rc_verification_method, 
         )
 
-        rc_pres_statements = {
-            # 'signature': {
-            #     'disclosed': ['anniversaryMonth']
-            # },
-            "revocation": {"accumulator": ""},
-            "encryption": {
-                "claim": "clientNo",
-                "domain": cc_demo.get("issuer").get("domain"),
-                "encryptionKey": cc_cred_def.get("verifiable_encryption_key"),
-            },
-        }
-        challenge = self.create_nonce().get("nonce")
-        rc_pres_schema = self.create_pres_schema(
-            rc_cred_def.get("id"), rc_pres_statements
-        ).get("presReq")
-        rc_presentation = self.create_presentation(
-            rc_credential, rc_pres_schema.get("id"), challenge
-        ).get("presentation")
-        rc_verified = self.verify_presentation(
-            rc_presentation, rc_pres_schema.get("id"), challenge
-        )
-        print(rc_cred_subject)
+        with open("app/static/demo/presentations/shoes-purchase.json", "r") as f:
+            shoes_demo = json.loads(f.read())
 
-        # # Decrypt Proof
-        # proofs = presentation.get('proofs')
-        # for proof_id in proofs:
-        #     proof = proofs.get(proof_id)
-        #     if proof.get('VerifiableEncryption'):
-        #         ve_proof = proof.get('VerifiableEncryption')
+        shoes_pres_schema_id = self.create_pres_schema(shoes_demo.get('query'))
 
-        # decryption_key = issuer_private.get('verifiable_decryption_key')
-        # decrypted_proof = self.decrypt_proof(ve_proof,decryption_key ).get('decrypted')
-        # decrypted_proof
+        with open("app/static/demo/presentations/shorts-purchase.json", "r") as f:
+            shorts_demo = json.loads(f.read())
 
-        demo = {
-            "credentials": {
-                "creditCard": {
-                    "issuer": {
-                        "name": "NeoVault",
-                        "domain": "flux@neovault.bank.example",
-                    },
-                    "credentialSubject": cc_cred_subject,
-                },
-                "rebatesCard": {
-                    "issuer": {
-                        "name": "SynergiPay Consortium",
-                        "domain": "rebates@synergipay.example",
-                    },
-                    "credentialSubject": rc_cred_subject,
-                },
-            }
-        }
+        shorts_pres_schema_id = self.create_pres_schema(shorts_demo.get('query'))
+        # challenge = self.create_nonce().get("nonce")
+        # cc_presentation = self.create_presentation(
+        #     cc_credential, cc_pres_schema.get("id"), challenge
+        # ).get("presentation")
+        # cc_verified = self.verify_presentation(
+        #     cc_presentation, cc_pres_schema.get("id"), challenge
+        # )
 
-        await askar.store("demo", "default", demo)
-        await askar.store("credential", "credit-card", cc_credential)
-        await askar.store("credential", "rebates-card", rc_credential)
-        await askar.store("presentation", "shoes-checkout", cc_presentation)
-        await askar.store("presentation", "shorts-checkout", rc_presentation)
+        # rc_pres_statements = {
+        #     # 'signature': {
+        #     #     'disclosed': ['anniversaryMonth']
+        #     # },
+        #     "revocation": {"accumulator": ""},
+        #     "encryption": {
+        #         "claim": "clientNo",
+        #         "domain": cc_demo.get("issuer").get("domain"),
+        #         "encryptionKey": cc_cred_def.get("verifiable_encryption_key"),
+        #     },
+        # }
+        # challenge = self.create_nonce().get("nonce")
+        # rc_pres_schema = self.create_pres_schema(
+        #     rc_cred_def.get("id"), rc_pres_statements
+        # ).get("presReq")
+        # rc_presentation = self.create_presentation(
+        #     rc_credential, rc_pres_schema.get("id"), challenge
+        # ).get("presentation")
+        # rc_verified = self.verify_presentation(
+        #     rc_presentation, rc_pres_schema.get("id"), challenge
+        # )
+        # print(rc_cred_subject)
+
+        # # # Decrypt Proof
+        # # proofs = presentation.get('proofs')
+        # # for proof_id in proofs:
+        # #     proof = proofs.get(proof_id)
+        # #     if proof.get('VerifiableEncryption'):
+        # #         ve_proof = proof.get('VerifiableEncryption')
+
+        # # decryption_key = issuer_private.get('verifiable_decryption_key')
+        # # decrypted_proof = self.decrypt_proof(ve_proof,decryption_key ).get('decrypted')
+        # # decrypted_proof
+
+        # demo = {
+        #     "credentials": {
+        #         "creditCard": {
+        #             "issuer": {
+        #                 "name": "NeoVault",
+        #                 "domain": "flux@neovault.bank.example",
+        #             },
+        #             "credentialSubject": cc_cred_subject,
+        #         },
+        #         "rebatesCard": {
+        #             "issuer": {
+        #                 "name": "SynergiPay Consortium",
+        #                 "domain": "rebates@synergipay.example",
+        #             },
+        #             "credentialSubject": rc_cred_subject,
+        #         },
+        #     }
+        # }
+
+        # await askar.store("demo", "default", demo)
+        # await askar.store("credential", "credit-card", cc_credential)
+        # await askar.store("credential", "rebates-card", rc_credential)
+        # await askar.store("presentation", "shoes-checkout", cc_presentation)
+        # await askar.store("presentation", "shorts-checkout", rc_presentation)
 
     def create_cred_schema(self, schema):
         r = requests.post(
-            f"{self.endpoint}/create-cred-schema",
+            f"{self.endpoint}/resources/schemas/credentials",
             json={"jsonSchema": schema, "options": {}},
         )
         return r.json()
 
-    def setup_issuer(self, schema_id):
+    def setup_issuer(self, issuer_name, schema_id):
         r = requests.post(
-            f"{self.endpoint}/setup-issuer", json={"options": {"schemaId": schema_id}}
+            f"{self.endpoint}/resources/issuers/{issuer_name}", json={"options": {"credSchemaId": schema_id}}
         )
         return r.json()
 
-    def issue_credential(self, cred_def_id, cred_subject):
+    def create_pres_schema(self, query):
         r = requests.post(
-            f"{self.endpoint}/issue-credential",
+            f"{self.endpoint}/resources/schemas/presentations",
+            json={"query": query},
+        )
+        return r.json()
+
+    def request_credential(self, subject_id, verification_method):
+        r = requests.post(
+            f"{self.endpoint}/operations/credentials/request",
             json={
-                "credentialSubject": cred_subject,
-                "options": {"credDefId": cred_def_id},
+                "subjectId": subject_id,
+                "verificationMethod": verification_method,
             },
         )
         return r.json()
 
-    def create_pres_schema(self, cred_def_id, pres_statements):
+    def issue_credential(self, cred_subject, cred_id, rev_id, verification_method, request_proof=None):
         r = requests.post(
-            f"{self.endpoint}/create-pres-schema",
-            json={"statements": pres_statements, "options": {"credDefId": cred_def_id}},
-        )
-        return r.json()
-
-    def create_nonce(self):
-        r = requests.post(
-            f"{self.endpoint}/utilities/challenge",
+            f"{self.endpoint}/operations/credentials/issuance",
+            json={
+                "credentialSubject": cred_subject,
+                "options": {
+                    "credentialId": cred_id,
+                    "revocationId": rev_id,
+                    "requestProof": request_proof,
+                    "verificationMethod": verification_method
+                },
+            },
         )
         return r.json()
 
@@ -167,6 +191,12 @@ class AnonCredsApi:
                 "credential": credential,
                 "options": {"challenge": challenge, "presReqId": pres_schema_id},
             },
+        )
+        return r.json()
+
+    def create_nonce(self):
+        r = requests.post(
+            f"{self.endpoint}/utilities/challenge",
         )
         return r.json()
 
